46948829a3bd9222d9f9fd36092fca978eeabc79
Anton Pashinov <troxin1988@gmail.com>
2024-10-19 16:18:18 +0700

    Edit logs.py

--- app1/src/schemas/logs.py
+++ app1/src/schemas/logs.py
@@ -1,23 +1,9 @@
 from datetime import datetime
-import re
-from pydantic import BaseModel, confloat, field_validator
+from schemas.query import QueryForServer
 
 
-class Log(BaseModel):
-    cadastral_number: str
-    latitude: float = confloat(ge=-90, le=90)
-    longitude: float = confloat(ge=-180, le=180)
+class Log(QueryForServer):
     response: bool
-
-    @field_validator("cadastral_number")
-    @classmethod
-    def validate_cadastral_number(cls, v):
-        pattern = r"^(?!:)[0-9:]*(?<!:)$"
-        if not re.match(pattern, v):
-            raise ValueError(
-                "Кадастровый номер должен начинаться и заканчиваться на цифру и может содержать только цифры и двоеточия"
-            )
-        return v
 
 
 class LogHistory(Log):
