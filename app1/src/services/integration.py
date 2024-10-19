4a8ab7110e864618aa4fbd738c620789ebc8208a
Anton Pashinov <troxin1988@gmail.com>
2024-10-19 17:07:22 +0700

    Integration with app2

--- app1/src/services/integration.py
+++ app1/src/services/integration.py
@@ -1,5 +1,6 @@
 from random import choice
 from typing import List
+import aiohttp
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from models.logs import Log
@@ -10,6 +11,10 @@ from schemas.query import QueryForServer
 async def write_log(log: Log, db: AsyncSession) -> None:
     """Пишет переданный лог в базу."""
 
+    print(type(log.cadastral_number))
+    print(type(log.latitude))
+    print(type(log.longitude))
+    print(type(log.response))
     db.add(log)
     await db.commit()
 
@@ -17,12 +22,16 @@ async def write_log(log: Log, db: AsyncSession) -> None:
 async def send_coords(query_data: QueryForServer, db: AsyncSession) -> bool:
     """Отправляет координаты на сторонний API."""
 
-    response = choice([True, False])
-
     data = query_data.model_dump()
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post("http://app2:8000/", json=data) as response:
+            response = await response.json()
+
     data["response"] = response
 
     await write_log(Log(**data), db)
+    
     return response
 
 
---
+++ app2/src/schemas/coords.py
@@ -0,0 +1,18 @@
+import re
+from pydantic import BaseModel, confloat, field_validator
+
+
+class Coords(BaseModel):
+    cadastral_number: str
+    latitude: confloat(ge=-90, le=90)
+    longitude: confloat(ge=-180, le=180)
+
+    @field_validator("cadastral_number")
+    @classmethod
+    def validate_cadastral_number(cls, v):
+        pattern = r"^(?!:)[0-9:]*(?<!:)$"
+        if not re.match(pattern, v):
+            raise ValueError(
+                "Кадастровый номер должен начинаться и заканчиваться на цифру и может содержать только цифры и двоеточия"
+            )
+        return v
